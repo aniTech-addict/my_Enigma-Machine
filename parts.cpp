0fea36a663c69b2259777b86e6b10a4029f9190c
Brad <kirti7666anand15@gmail.com>
2025-06-17 23:54:49 +0530

    ahhh

--- parts.cpp
+++ parts.cpp
@@ -6,3 +6,11 @@ class rotors{
     vector<vector<char>> rotors;
     vector 
 };
+
+class keyboard{
+    vector<char> keyboard={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+};
+
+class plugBoard{
+    
+};
