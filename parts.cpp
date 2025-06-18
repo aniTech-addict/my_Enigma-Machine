61971949a8c40823ad9fa8b3564d931d1e2630ff
Brad <kirti7666anand15@gmail.com>
2025-06-18 07:15:27 +0530

    base enigma

--- parts.cpp
+++ parts.cpp
@@ -1,15 +1,51 @@
-// Individual Parts of the Enigma Machine
-#include <vectors>
 #include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <string>
+#include <set>
+using namespace std;
+class Rotor {
+public:
+    string wiring;        // 26-char string
+    char notch;           // // Turning point for other rotors
+    int position;         // record the current position 
+    int ringSetting;      // Setting up the Rotor ring's first value 
 
-class rotors{
-    vector<vector<char>> rotors;
+    Rotor (char notch,int ringSetting){
+        this->notch = notch;
+        this->position = ringSetting;
+        this->wiring = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+    void step(){
+        position = (position + 1) % 26;
+    };
+    bool atNotch(){
+        return position == notch - 'A';
+    };
 };
 
-class keyboard{
-    vector<char> keyboard={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+class Reflector {
+public:
+    string wiring; // 26-char string
+    char reflect(char c);
 };
 
-class plugBoard{
+class Plugboard {
+public:
+    unordered_map<char, char> mapping;
+    char swap(char c);
+};
+
+class EnigmaMachine {
+public:
+    Plugboard plugboard;
+    Rotor left(E,0);
+    Rotor middle(K,4);
+    Rotor left(F,8);
+
     
+    Reflector reflector;
+
+    char encryptChar(char c);
+    string encryptMessage(string msg);
 };
